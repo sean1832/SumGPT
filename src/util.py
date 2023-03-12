14d6522ce82e092fe89c4eb5bde2f7e4d180f47d
sean1832 <sean1832725142@gmail.com>
2023-03-13 02:34:05 +1100

    feat: add language detection for separate operation between latin based language and east asian language

--- src/util.py
+++ src/util.py
@@ -4,6 +4,8 @@ from GPT.embeddings import openAIEmbeddings
 import streamlit as st
 import re
 import GPT
+import textwrap
+from langdetect import detect
 
 
 def similarity(v1, v2) -> np.ndarray:
@@ -11,39 +13,55 @@ def similarity(v1, v2) -> np.ndarray:
     return np.dot(v1, v2)
 
 
-def _chunk_spliter(content: str, chunk_size: int = 1000) -> List[str]:
+def language_base(string: str):
+    lang_code = detect(string)
+    latin_based = ['en', 'fr-ca', 'es']
+    east_asian_based = ['zh', 'ja', 'ko']
+    for lang in latin_based:
+        if lang_code.startswith(lang):
+            return 'latin'
+    for lang in east_asian_based:
+        if lang_code.startswith(lang):
+            return 'east_asian'
+    return 'other'
+
+
+def _chunk_spliter(content: str, chunk_size: int = 1000, lang_base: str = 'latin') -> List[str]:
     """Splits a string into chunks of a given size."""
 
     sentences = re.split(r'(?<=[.?!,。，、！？·])\s+', content)
-    chunks = []
-    chunk = ''
-    word_count = 0
-    for sentence in sentences:
-        sentence += ' '  # add space at end to compensate for split
-        words = sentence.split()
-        sentence_word_count = len(words)
-        if word_count + sentence_word_count <= chunk_size:
-            chunk += sentence
-            word_count += sentence_word_count
-        else:
+    if lang_base == 'latin':
+        chunks = []
+        chunk = ''
+        word_count = 0
+        for sentence in sentences:
+            sentence += ' '  # add space at end to compensate for split
+            words = sentence.split()
+            sentence_word_count = len(words)
+            if word_count + sentence_word_count <= chunk_size:
+                chunk += sentence
+                word_count += sentence_word_count
+            else:
+                chunks.append(chunk.strip())
+                chunk = sentence
+                word_count = sentence_word_count
+        if chunk:
             chunks.append(chunk.strip())
-            chunk = sentence
-            word_count = sentence_word_count
-    if chunk:
-        chunks.append(chunk.strip())
+    else:
+        chunks = textwrap.wrap(content, width=chunk_size)
     return chunks
 
 
 def convert_to_chunks(content: str, chunk_size: int = 1000, enable_embedding: bool = False) -> List[Dict[str, float]]:
     """Converts a string into chunks of a given size."""
-    chunks_text = _chunk_spliter(content, chunk_size)
+    chunks_text = _chunk_spliter(content, chunk_size, language_base(content))
     chunks = []
     for i, chunk in enumerate(chunks_text):
         if enable_embedding:
             embedding = openAIEmbeddings(st.session_state["OPENAI_API_KEY"])
             chunks.append({'content': chunk, 'vector': embedding.embedding(chunk)})
         else:
-            chunks.append({'content': chunk, 'vector': None})
+            chunks.append({'content': chunk, 'vector': None, 'language_based': language_base(chunk)})
     return chunks
 
 
@@ -71,7 +89,8 @@ def recursive_summarize(chunks: List[Dict[str, float]]) -> Tuple[List[str], str]
     progress_bar = st.progress(0)
     for chunk in chunks:
         content = chunk['content']
-        text, finish_reason = GPT.generate.get_answer(content, recursive=True, persona=st.session_state['OPENAI_PERSONA_REC'])
+        text, finish_reason = GPT.generate.get_answer(content, recursive=True,
+                                                      persona=st.session_state['OPENAI_PERSONA_REC'])
         recursiveSumTexts.append(text)
         progress_bar.progress((count + 1) / chunks_length)
         count += 1
@@ -81,6 +100,6 @@ def recursive_summarize(chunks: List[Dict[str, float]]) -> Tuple[List[str], str]
 def summarize(recursive_sum: List[str]) -> Tuple[str, str]:
     """Returns a summary of the given content."""
     join_sum = ' '.join(recursive_sum)
-    answer, finish_reason = GPT.generate.get_answer(join_sum, recursive=False, persona=st.session_state['OPENAI_PERSONA_SUM'])
+    answer, finish_reason = GPT.generate.get_answer(join_sum, recursive=False,
+                                                    persona=st.session_state['OPENAI_PERSONA_SUM'])
     return answer, finish_reason
-
